e9a4cef28455261391fccd33f66f69eced46fdd2
DDS-GH <delldesignsystem@dell.com>
2022-03-30 15:48:12 -0500

    chore: polishing modal

--- src/app/pages/modal/modal.component.ts
+++ src/app/pages/modal/modal.component.ts
@@ -1,12 +1,22 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: "app-modal",
-  templateUrl: "./modal.component.html"
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
 })
 export class ModalPageComponent {
-  @ViewChild(`cheeseModal`) cheeseModal: ElementRef<HTMLElement>
-  openModal(e: any) {
+  @ViewChild(`cheeseModal`) cheeseModal: ElementRef<HTMLElement>;
+
+  open(e: any) {
     this.cheeseModal[`ddsComponent`].open();
+  }
+
+  close(e: any) {
+    if (e.target.innerText.match(/non/gi)) {
+      console.info(`Non`);
+    } else {
+      console.info(`Oui`);
+    }
+    this.cheeseModal[`ddsComponent`].close();
   }
 }
